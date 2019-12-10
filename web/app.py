6b306438bca96cc6d5e80d562f2e9908b3d92654
ELGUMOS1 <54557959+ELGUMOS1@users.noreply.github.com>
2019-12-10 17:11:10 -0600

    Update app.py

--- web/app.py
+++ web/app.py
@@ -6,7 +6,7 @@ from config import BaseConfig
 ################
 #### config ####
 ################
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
 
