a42fe3963f60091b3c8f0be5ee941e353654b915
ELGUMOS1 <54557959+ELGUMOS1@users.noreply.github.com>
2019-12-10 15:48:18 -0600

    Update test_app.py

--- web/test_app.py
+++ web/test_app.py
@@ -1,35 +1,44 @@
-from app import app
+
+
+import os
 import unittest
 
-class FlaskAppTests(unittest.TestCase):
+from . import app, db
 
-    @classmethod
-    def setUpClass(cls):
-        pass
 
-    @classmethod
-    def tearDownClass(cls):
-        pass
+TEST_DB = 'test.db'
 
+
+class BasicTests(unittest.TestCase):
+
+    ############################
+    #### setup and teardown ####
+    ############################
+
+    # executed prior to each test
     def setUp(self):
+        app.config['TESTING'] = True
         
-        # creates a test client
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+            os.path.join(app.config['BASEDIR'], TEST_DB)
         self.app = app.test_client()
-        # propagate the exceptions to the test client
-        self.app.testing = True
 
+    # executed after each test
     def tearDown(self):
         pass
-    
-    def test_fill_database(self):
-        result = self.app.post('/')
+
+
+    def test_database(self):
+        result = self.app.get('/')
         self.assertEqual(result.status_code, 200)
         
-    def test_eight_queen_puzzle(self):
-      
+    def test_eight_queen_puzzle(self)  
         result = self.app.get('/get/4')
-        #print result.data
-        
+        #print result.data     
         self.assertIn('[[1, 3, 0, 2], [2, 0, 3, 1]]',result.data)
 
-   
+  
+if __name__ == "__main__":
+    unittest.main()
