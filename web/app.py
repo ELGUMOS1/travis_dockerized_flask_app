79154ccc922920cd552c570dbd2de1a6a6d2790b
ELGUMOS1 <54557959+ELGUMOS1@users.noreply.github.com>
2019-12-10 17:04:02 -0600

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,7 +1,7 @@
 from os.path import join, isfile
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-
+from config import BaseConfig
 
 ################
 #### config ####
