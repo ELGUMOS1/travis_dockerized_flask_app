79ce02a51d342ca17b90ef874f0af6faed6c9614
ELGUMOS1 <54557959+ELGUMOS1@users.noreply.github.com>
2019-12-11 13:26:39 -0600

    Update test_app.py

--- web/test_app.py
+++ web/test_app.py
@@ -47,7 +47,7 @@ class BasicTests(unittest.TestCase):
     def test_eight_queen_puzzle_8(self):  
         result = self.app.get('/get/8') 
         l = list(result.data.decode())
-        self.assertEqual(len(l), 92)
+        self.assertEqual(len(l)[0], 92)
         
 
   
