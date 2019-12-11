32ad10cfafdb8e1fb3f920abb75d4486e5aa0b16
ELGUMOS1 <54557959+ELGUMOS1@users.noreply.github.com>
2019-12-11 11:22:39 -0600

    Update models.py

--- web/models.py
+++ web/models.py
@@ -7,7 +7,7 @@ class Post(db.Model):
     __tablename__ = 'solutions'
 
     n = db.Column(db.Integer, primary_key=True)
-    sol = db.Column(db.String, nullable=False)
+    sol = db.Column(db.String())
 
     def __init__(self, n, sol):
         self.sol = sol
