0883f728db024b1b9b8d6b5765744f60244d3549
ELGUMOS1 <54557959+ELGUMOS1@users.noreply.github.com>
2019-12-11 14:35:56 -0600

    Update config.py

--- web/config.py
+++ web/config.py
@@ -1,8 +1,6 @@
 # config.py
 
-
 import os
-
 
 class BaseConfig(object):
     SECRET_KEY = os.environ['SECRET_KEY']
@@ -15,14 +13,3 @@ class BaseConfig(object):
     SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
         DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
     )
-
-
-# class BaseConfig(object):
-#     SECRET_KEY = 'hi'
-#     DEBUG = True
-#     DB_NAME = 'postgres'
-#     DB_SERVICE = 'localhost'
-#     DB_PORT = 5432
-#     SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}/{2}'.format(
-#         DB_SERVICE, DB_PORT, DB_NAME
-#     )
